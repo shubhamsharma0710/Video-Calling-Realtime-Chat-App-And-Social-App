ca649cf2642018a4b4ecdd5bf9a5e63ad46d1b45
shubham <84614805+shubhamsharma0710@users.noreply.github.com>
2026-01-08 23:28:10 +0530

    Update BASE_URL for development and production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://setuchatandvideocall.onrender.com" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://setuchatandvideocall.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, 
+  withCredentials: true, // 🔴 REQUIRED
 });
