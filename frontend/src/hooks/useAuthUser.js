3be64c539f5139f0032f4919297baf6be7149918
shubham <84614805+shubhamsharma0710@users.noreply.github.com>
2026-01-09 20:50:15 +0530

    Refactor useAuthUser hook to destructure query result

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -2,12 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getAuthUser } from "../lib/api";
 
 const useAuthUser = () => {
-  const authUser = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["authUser"],
     queryFn: getAuthUser,
     retry: false, 
   });
 
-  return { isLoading: authUser.isLoading, authUser: authUser.data?.user };
+  return {
+    isLoading,
+    authUser: data ? data.user : null,
+  };
 };
+
 export default useAuthUser;
